735925f0fa5007df8c57fb0e450ca2cd19f1fa9a
Znichu <znichu1989@gmail.com>
2020-06-01 00:46:38 +0300

    Updates

---
+++ precache-manifest.7b0c75bedc4a0cae078e371d29d404ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9434435352bd08e5eb4e1ba4798e9f",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "c817f4f34c57c25325c7",
+    "url": "/counter/static/css/main.ec922251.chunk.css"
+  },
+  {
+    "revision": "841bae42383bb2a20735",
+    "url": "/counter/static/js/2.13feb95f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.13feb95f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c817f4f34c57c25325c7",
+    "url": "/counter/static/js/main.221ac1c6.chunk.js"
+  },
+  {
+    "revision": "784d33e82c93a030f1f4",
+    "url": "/counter/static/js/runtime-main.e0439584.js"
+  }
+]);
